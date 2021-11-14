e3a16fa18e50f25da37be2a4201de9224f0a06f2
robot256 <robot256@GMAIL.COM>
2021-11-14 10:09:04 -0500

    Update settings.lua
    Correct file this time

--- settings.lua
+++ settings.lua
@@ -1,47 +1,32 @@
---[[ Copyright (c) 2019 robot256 (MIT License)
- * Project: Multiple Unit Train Control
- * File: settings.lua
- * Description: Setting to control MU operation.
---]]
 
 data:extend{
   {
-    type = "string-setting",
-    name = "multiple-unit-train-control-mode",
+    type = "bool-setting",
+    name = "ltn-opts-remove-temporary-stops",
     order = "aa",
     setting_type = "runtime-global",
-    default_value = "tech-unlock",
-    allowed_values = {"disabled","basic","advanced","tech-unlock"}
-  },
-  {
-    type = "int-setting",
-    name = "multiple-unit-train-control-on_nth_tick",
-    order = "ab",
-    setting_type = "runtime-global",
-    minimum_value = 0,
-    default_value = 300
-  },
-  {
-    type = "string-setting",
-    name = "multiple-unit-train-control-debug",
-    order = "ac",
-    setting_type = "runtime-global",
-    default_value = "info",
-    allowed_values = {"none","error","info","debug"}
+    default_value = false,
   },
   {
     type = "bool-setting",
-    name = "multiple-unit-train-control-allow_yuoki_steam",
-    order = "aa",
-    setting_type = "startup",
-    default_value = false
+    name = "ltn-opts-remove-temporary-directions",
+    order = "ab",
+    setting_type = "runtime-global",
+    minimum_value = 0,
+    default_value = false,
   },
   {
-    type = "string-setting",
-    name = "multiple-unit-train-control-blacklist",
-    order = "ab",
-    setting_type = "startup",
-    default_value = "",
-    allow_blank = true
+    type = "bool-setting",
+    name = "ltn-opts-remove-temporary-directions-smart",
+    order = "ac",
+    setting_type = "runtime-global",
+    default_value = true,
+  },
+  {
+    type = "bool-setting",
+    name = "ltn-opts-debug-print",
+    order = "ad",
+    setting_type = "runtime-global",
+    default_value = true,
   },
 }
