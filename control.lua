dd24460e3eab281da748efb13c314aec344c8ad5
robot256 <robot256@GMAIL.COM>
2021-11-17 07:30:03 -0500

    Changed "smart" mode
    - Smart mode now looks for a train stop on the other end of the LTN stop's block.
    - If the other end has an exiting train stop and it is connected to the LTN train stop  by a wire (NOT the lamp or output), then the temporary stop is made bidirectional and moved to the other train stop.
    - Note that the other train stop needs to be one tile farther than it would be if you were actually stopping at it, so that the train actually has to travel some distance to reverse into the LTN stop.

--- control.lua
+++ control.lua
@@ -26,6 +26,16 @@ local function ReloadSettings()
   
 end
 
+
+local function AreConnected(a, b)
+  return (a.get_circuit_network(defines.wire_type.red) and b.get_circuit_network(defines.wire_type.red) and 
+           (a.get_circuit_network(defines.wire_type.red).network_id == b.get_circuit_network(defines.wire_type.red).network_id)
+         ) or
+         (a.get_circuit_network(defines.wire_type.green) and b.get_circuit_network(defines.wire_type.green) and 
+           (a.get_circuit_network(defines.wire_type.green).network_id == b.get_circuit_network(defines.wire_type.green).network_id)
+         )
+end
+
 local function ProcessSchedule(schedule)
   
   -- Scan schedule for rail_record stops.
@@ -59,25 +69,28 @@ local function ProcessSchedule(schedule)
       end
     
     elseif remove_temporary_directions_smart then
-      -- Remove the direction flag from every temporary stop IF there are TWO stops attached to this rail with the same name
+      -- Check if there is a train stop at the other end of this LTN stops' rail segment that is wired to the LTN stop
       for i = 1, #schedule.records, 1 do
         if not schedule.records[i].station and schedule.records[i].rail and schedule.records[i].rail_direction then  -- found rail stop
-          local rail = schedule.records[i].rail
           if i < #schedule.records and schedule.records[i+1].station then  -- followed by station
-            local station = schedule.records[i+1].station
-            -- find these stations nearby
-            local stops = rail.surface.find_entities_filtered({type="train-stop", position=rail.position, radius=2})
-            if stops then
-              if debug_print then
-                game.print("Found "..#stops.." stops near rail at ("..tostring(rail.position.x)..","..tostring(rail.position.y)..")")
+            local rail = schedule.records[i].rail
+            local front_entity = rail.get_rail_segment_entity(defines.rail_direction.front, false)
+            local back_entity = rail.get_rail_segment_entity(defines.rail_direction.back, false)
+            if front_entity and front_entity.type == "train-stop" and back_entity and back_entity.type == "train-stop" and AreConnected(front_entity, back_entity) then
+              -- block is bounded by train stops
+              local station = schedule.records[i+1].station
+              local waypoint_rail = nil
+              if (front_entity.name == "logistic-train-stop" and front_entity.backer_name == station) then
+                waypoint_rail = back_entity.connected_rail
+              elseif (back_entity.name == "logistic-train-stop" and back_entity.backer_name == station) then
+                waypoint_rail = front_entity.connected_rail
               end
-              if #stops == 2 and 
-                stops[1].backer_name == station and stops[2].backer_name == station and 
-                stops[1].connected_rail == rail and stops[2].connected_rail == rail then
-                if debug_print then
-                  game.print("Smart removing direction of temporary stop at station "..station)
-                end
+              if waypoint_rail then
+                schedule.records[i].rail = waypoint_rail
                 schedule.records[i].rail_direction = nil
+                if debug_print then
+                  game.print("Moved waypoint to new location ("..tostring(waypoint_rail.position.x)..","..tostring(waypoint_rail.position.y)..")")
+                end
                 changed = true
               end
             end
@@ -112,13 +125,21 @@ function OnDispatcherUpdated(event)
   global.lastDispatchTick = game.tick
 end
 
+function OnStopsUpdated(event)
+  if event.logistic_train_stops then
+    global.LogisticTrainStops = event.logistic_train_stops
+  end
+end
+
 
 local function initGlobals()
   global.lastDispatchTick = global.lastDispatchTick or 0
+  global.LogisticTrainStops = global.LogisticTrainStops or {}
 end
 
 local function registerEvents()
   script.on_event(remote.call("logistic-train-network", "on_dispatcher_updated"), OnDispatcherUpdated)
+  script.on_event(remote.call("logistic-train-network", "on_stops_updated"), OnStopsUpdated)
 end
 
 script.on_event(defines.events.on_runtime_mod_setting_changed, function()
